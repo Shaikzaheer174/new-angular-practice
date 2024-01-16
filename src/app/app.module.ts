79667b9a45e5e11ddedcb9de9ca84a4387402e45
Shaikzaheer174 <shaikzaheer174@gmail.com>
2024-01-16 23:14:08 +0530

    property binding example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
 import { TestComponent } from './test/test.component';
+import { PropertyBindingExampleComponent } from './property-binding-example/property-binding-example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentComponent,
-    TestComponent
+    TestComponent,
+    PropertyBindingExampleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/property-binding-example/property-binding-example.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-binding-example',
+  templateUrl: './property-binding-example.component.html',
+  styleUrls: ['./property-binding-example.component.css']
+})
+export class PropertyBindingExampleComponent {
+courseID = 'id1'; 
+disabledText = true;
+}
